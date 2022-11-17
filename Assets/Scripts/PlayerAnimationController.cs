d382dc119953ace972b9b30895faa9f109c0f270
Tiago Braga <braga.tiagoxavier@gmail.com>
2022-11-17 13:33:48 -0300

    Create animation

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class PlayerAnimationController : MonoBehaviour
+{
+    private PlayerController player;
+    [SerializeField] private Animator animator; 
+
+    private void Awake() {
+        player = GetComponent<PlayerController>();
+    }
+    private void Update() {
+        animator.SetBool("IsJumping", player.IsJumping);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,23 +4,83 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float horizontalSpeed = 0.03f;
-    [SerializeField] private float forwardSpeed = 0.05f;
+    [SerializeField] private float horizontalSpeed = 15;
+    [SerializeField] private float forwardSpeed = 10;
+    [SerializeField] private float laneDistanceX = 2;
+
+    [SerializeField] private float jumpDistanceZ = 5;
+    [SerializeField] private float jumpHeightY = 2;
+
+    Vector3 initialPosition;
+
+    float targetPositionX;
+    float jumpStartZ;
+    public bool IsJumping { get; private set;}
+
+    private float LeftLaneX => initialPosition.x - laneDistanceX;
+    private float RightLaneX => initialPosition.x + laneDistanceX;
+    
+    private void Awake() {
+        initialPosition = transform.position;
+    }
+
     private void Update()
     {
-        Vector3 targetPosition = transform.position;
-        if (Input.GetKey(KeyCode.A))
-        {
-            targetPosition += Vector3.left * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            targetPosition += Vector3.right * Time.deltaTime;
-        }
+        ProcessInput();
 
-        targetPosition += Vector3.forward * forwardSpeed;
+        Vector3 position = transform.position;
 
-        transform.position = targetPosition;
+        position.x = ProcessLaneMovement();
+        position.y = ProcessJump();
+        position.z = ProcessForwardMovement();
+
+        transform.position = position;
     }    
 
+    private void ProcessInput()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            targetPositionX += laneDistanceX;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            targetPositionX -= laneDistanceX;
+        }
+        if (Input.GetKeyDown(KeyCode.W) && !IsJumping)
+        {
+            IsJumping = true;
+            jumpStartZ = transform.position.z;
+        }
+
+        targetPositionX = Mathf.Clamp(targetPositionX, LeftLaneX, RightLaneX);
+    }
+
+    private float ProcessLaneMovement()
+    {
+        return Mathf.Lerp(transform.position.x, targetPositionX, Time.deltaTime * horizontalSpeed);
+    }
+
+    private float ProcessForwardMovement()
+    {
+        return transform.position.z + forwardSpeed * Time.deltaTime;
+    }
+    
+    private float ProcessJump()
+    {
+        float deltaY = 0;
+        if (IsJumping)
+        {
+            float jumpPercent = (transform.position.z - jumpStartZ) / jumpDistanceZ;
+            if (jumpPercent >= 1)
+            {
+                IsJumping = false;
+            } 
+            else
+            {
+                deltaY = Mathf.Sin(Mathf.PI*jumpPercent) * jumpHeightY;
+            }
+        }
+        return initialPosition.y + deltaY;
+    }
 }
